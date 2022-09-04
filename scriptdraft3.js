6aa65e666fbc3f0d1944ad6eb283a45b136b5b4f
ao donovan <a.o.donovan.music@gmail.com>
2022-09-04 18:30:08 -0400

    add numpad functionality

--- scriptdraft3.js
+++ scriptdraft3.js
@@ -5,12 +5,108 @@ let funcVarVal = "addition";
 let reset = false;
 let outputWindow = document.getElementById("output");
 let numBtns = document.querySelectorAll(".numPad");
+window.addEventListener("keydown", keyListen);
 document.getElementById("add").addEventListener("click", plusOp);
 document.getElementById("equals").addEventListener("click", operate);
 document.getElementById("subtract").addEventListener("click", minusOp);
 document.getElementById("multiply").addEventListener("click", multiplyOp)
 document.getElementById("divide").addEventListener("click", divideOp);
 numBtns.forEach(element => element.addEventListener("click", logBtn));
+function keyListen(e){
+    console.log(e.code);
+    if(e.code == "Numpad1"){
+        inputString = inputString + "1";
+        let newEntry = document.createElement("p");
+        newEntry.innerHTML = "1";
+        document.getElementById("output").appendChild(newEntry);
+        currentVal = parseFloat(inputString);
+    }
+    if(e.code == "Numpad2"){
+        inputString = inputString + "2";
+        let newEntry = document.createElement("p");
+        newEntry.innerHTML = "2";
+        document.getElementById("output").appendChild(newEntry);
+        currentVal = parseFloat(inputString);
+    }
+    if(e.code == "Numpad3"){
+        inputString = inputString + "3";
+        let newEntry = document.createElement("p");
+        newEntry.innerHTML = "3";
+        document.getElementById("output").appendChild(newEntry);
+        currentVal = parseFloat(inputString);
+    }
+    if(e.code == "Numpad4"){
+        inputString = inputString + "4";
+        let newEntry = document.createElement("p");
+        newEntry.innerHTML = "4";
+        document.getElementById("output").appendChild(newEntry);
+        currentVal = parseFloat(inputString);
+    }
+    if(e.code == "Numpad5"){
+        inputString = inputString + "5";
+        let newEntry = document.createElement("p");
+        newEntry.innerHTML = "5";
+        document.getElementById("output").appendChild(newEntry);
+        currentVal = parseFloat(inputString);
+    }
+    if(e.code == "Numpad6"){
+        inputString = inputString + "6";
+        let newEntry = document.createElement("p");
+        newEntry.innerHTML = "6";
+        document.getElementById("output").appendChild(newEntry);
+        currentVal = parseFloat(inputString);
+    }
+    if(e.code == "Numpad7"){
+        inputString = inputString + "7";
+        let newEntry = document.createElement("p");
+        newEntry.innerHTML = "7";
+        document.getElementById("output").appendChild(newEntry);
+        currentVal = parseFloat(inputString);
+    }
+    if(e.code == "Numpad8"){
+        inputString = inputString + "8";
+        let newEntry = document.createElement("p");
+        newEntry.innerHTML = "8";
+        document.getElementById("output").appendChild(newEntry);
+        currentVal = parseFloat(inputString);
+    }
+    if(e.code == "Numpad9"){
+        inputString = inputString + "9";
+        let newEntry = document.createElement("p");
+        newEntry.innerHTML = "9";
+        document.getElementById("output").appendChild(newEntry);
+        currentVal = parseFloat(inputString);
+    }
+    if(e.code == "Numpad0"){
+        inputString = inputString + "0";
+        let newEntry = document.createElement("p");
+        newEntry.innerHTML = "0";
+        document.getElementById("output").appendChild(newEntry);
+        currentVal = parseFloat(inputString);
+    }
+    if(e.code == "NumpadDecimal"){
+        inputString = inputString + ".";
+        let newEntry = document.createElement("p");
+        newEntry.innerHTML = ".";
+        document.getElementById("output").appendChild(newEntry);
+        currentVal = parseFloat(inputString);
+    }
+    if(e.code == "NumpadEnter"){
+        operate();
+    }
+    if(e.code == "NumpadAdd"){
+        plusOp();
+    }
+    if(e.code == "NumpadMultiply"){
+        multiplyOp();
+    }
+    if(e.code == "NumpadSubtract"){
+        minusOp();
+    }
+    if(e.code == "NumpadDivide"){
+        divideOp();
+    }
+}
 function logBtn(){
     if(reset == true){
         inputString = "";
